ae69c313cedef88f8bae031e22e2e56a13ed3ca2
lalzona-byte <lalzona@ucsc.edu>
2025-10-02 12:12:40 -0700

    Update code-journal-1.py

--- code-journal-1.py
+++ code-journal-1.py
@@ -7,5 +7,5 @@ favorite_food = "pizza"
 
 print("name:", name)
 print("pronouns:", pronouns)
-print(f"One of my absolute favorite movies is '{favorite_movie}'. I just watches it recently and it's always amazing.")
-print(f"one of my favorite foods is {pizza}. Though very basic, it's an absolute classic.")
+print(f"One of my absolute favorite movies is '{favorite_movie}'. I just watched it recently and it's always amazing.")
+print(f"one of my favorite foods is {favorite_food}. Though very basic, it's an absolute classic.")
