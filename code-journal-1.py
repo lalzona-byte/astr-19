6478cec4f6c6e0287052af97578d8d75359cebd8
lalzona-byte <lalzona@ucsc.edu>
2025-10-02 12:07:04 -0700

    Coding Journal Prompt #1

---
+++ code-journal-1.py
@@ -0,0 +1,11 @@
+# session 1 prompt
+# print name, pronouns, and favorite movie and food 
+name = "Lo Alzona"
+pronouns = "she/her"
+favorite_movie = "Pulp Fiction" 
+favorite_food = "pizza"  
+
+print("name:", name)
+print("pronouns:", pronouns)
+print(f"One of my absolute favorite movies is '{favorite_movie}'. I just watches it recently and it's always amazing.")
+print(f"one of my favorite foods is {pizza}. Though very basic, it's an absolute classic.")
