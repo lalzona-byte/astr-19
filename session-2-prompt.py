03561a890e1bbae5512d0d7306e9f374afadebae
lalzona-byte <lalzona@ucsc.edu>
2025-10-03 16:42:46 -0700

    Session 2 upload

---
+++ session-2-prompt.py
@@ -0,0 +1,28 @@
+#print the sum of two floating point numbers 
+#print the difference between two integers 
+#print the product of a floating number and an interger 
+#print data type in each case as well 
+
+import numpy as np 
+
+x = 7.0 #set variables of floats
+y = 6.0 
+answer = x+y #addition operators 
+
+print("x+y =", answer)
+print("data type:", type(answer)) #print data type 
+
+
+x = 10 #set variables of integers 
+y = 4 
+sub_answer = x-y 
+print("x-y =", sub_answer) #subtraction operators 
+print("data type:", type(sub_answer)) #print data type 
+
+i = 25  #set integer and float 
+f = 2.0 
+int_float = i*f #multiplication operator 
+print("i*f=", int_float)
+print("data type:", type(int_float)) #print date type 
+
+
